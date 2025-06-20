a6788a6d8f52249d2f8f3a1dd0d502e5ea8105fd
jakob miesner <jakob.miesner@gmail.com>
2025-06-20 16:19:30 +0200

    tests: added github action for running tests

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2025 CERN.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
 
 npx markdownlint-cli docs/* && \
 awesome_bot --allow-dupe --skip-save-results --allow-redirect docs/**/*.md && \
